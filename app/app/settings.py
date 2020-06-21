bc175acaa55f79c42a5b5d73f8dcb0a38ec6a7e7
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-06-22 02:05:03 +1000

    configured postgresql docker environment in app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# we configure postgresql from DOCKERFILE environment variables
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
     }
 }
 
